dfec71da9ddc89258abbc9b53359aa4faefa404c
yuriy_beloff <paganismrus@gmail.com>
2018-07-06 09:38:38 +0300

    added POST processing

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from typograf import apply_rule_to_test
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def form():
-    return render_template('form.html')
+    if request.method == 'POST':
+        input_text = request.form.get('text')
+        if not input_text:
+            print('Ничего не введено')
+        output_text = apply_rule_to_test(input_text)
+        return render_template(
+            'form.html',
+            raw_text=input_text,
+            processed_text=output_text)
+    else:
+        return render_template('form.html')
 
 if __name__ == "__main__":
     app.run()
