1771bebe35198502241ce30fcfaf1b5d1eb8e8f6
yuriy_beloff <paganismrus@gmail.com>
2018-07-06 09:58:29 +0300

    repaired mark in function name

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from typograf import apply_rule_to_test
+from typograf import apply_rule_to_text
 app = Flask(__name__)
 
 
@@ -9,7 +9,7 @@ def form():
         input_text = request.form.get('text')
         if not input_text:
             print('Ничего не введено')
-        output_text = apply_rule_to_test(input_text)
+        output_text = apply_rule_to_text(input_text)
         return render_template(
             'form.html',
             raw_text=input_text,
